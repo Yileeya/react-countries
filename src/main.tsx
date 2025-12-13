ce8586aa588600a6e3757abda036109ae23ccb65
yilee <yileefrontend@gmail.com>
2025-12-13 20:43:20 +0800

    chore(deps): Install and integrate @tanstack/react-query-devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,28 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import {BrowserRouter} from 'react-router-dom';
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient({
-    // 可以設置預設的快取時間等配置，這裡先使用基本配置
-    defaultOptions: {
-        queries: {
-            retry: 0,
-            staleTime: 1000 * 60 * 5 // 數據 5 分鐘後才視為過期
-        }
-    }
+  // 可以設置預設的快取時間等配置，這裡先使用基本配置
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      staleTime: 1000 * 60 * 5, // 數據 5 分鐘後才視為過期
+    },
+  },
 });
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <BrowserRouter>
-                <App/>
-            </BrowserRouter>
-        </QueryClientProvider>
-    </StrictMode>
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </StrictMode>,
 );
