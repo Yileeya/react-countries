765bd63f7b6f73abc80c609d46f0d74b7ce151c8
yilee <yileefrontend@gmail.com>
2025-12-13 10:50:44 +0800

    feat: integrate @tanstack/react-query

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,23 @@ import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import {BrowserRouter} from 'react-router-dom';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+    // 可以設置預設的快取時間等配置，這裡先使用基本配置
+    defaultOptions: {
+        queries: {
+            staleTime: 1000 * 60 * 5 // 數據 5 分鐘後才視為過期
+        }
+    }
+});
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </QueryClientProvider>
     </StrictMode>
 );
